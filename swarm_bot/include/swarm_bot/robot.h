931173ecdfce1c0c57ae84978b43b56b419b3a11
cxnf <rommen_95@hotmail.com>
2013-03-21 10:44:53 +0100

    initial source code

---
+++ swarm_bot/include/swarm_bot/robot.h
@@ -0,0 +1,90 @@
+#pragma once
+
+#include <string>
+#include "ros/ros.h"
+
+//! Robot class.
+/*!
+  Logical representation of a robot.
+ */
+class Robot
+{
+ private:
+  int32_t staticID;                               // static id by which a robot is known
+  int32_t dynamicID;                              // dynamic id which positions a robot in the formation
+  int32_t heartbeat;                              // time passed since last heartbeat
+  int32_t missed;                                 // amount of heartbeats missed in succession
+  std::string name;                               // robot name
+
+ public:
+  //! Default constructor.
+  /*!
+    Constructor which zeroes out each member, causing StaticID to be invalid.
+   */
+  Robot();
+  //! Main constructor.
+  /*!
+    Constructor which initializes the class with provided data.
+    /param n Robot name, must be unique.
+    /param sid StaticID assigned to robot, must be unique.
+   */
+  Robot(std::string n, int32_t sid);
+
+  //! Returns StaticID.
+  /*!
+    Returns the StaticID which is assigned to this robot.
+    Must be greater than 0 to be valid.
+    \return StaticID of robot.
+   */
+  int32_t GetStaticID();
+
+  //! Returns DynamicID.
+  /*!
+    Returns the DynamicID which is assigned to this robot.
+    Must be greater than 0 to be valid.
+    \return DynamicID of robot.
+   */
+  int32_t GetDynamicID();
+
+  //! Returns robot name.
+  /*!
+    Returns the name of the robot.
+    \return Name of robot.
+   */
+  std::string GetName();
+
+  //! Returns Alive state.
+  /*!
+    Returns a value indicating if the actual robot keeps its StaticID alive.
+    \return Value indicating StaticID is alive.
+   */
+  bool IsAlive();
+
+  //! Returns missed heartbeats.
+  /*!
+    Returns the total amount of heartbeats missed in succession.
+    \return Missed heartbeats since last heartbeat.
+   */
+  int32_t GetMissedHeartbeats();
+
+  //! Updates StaticID state and returns if state changed.
+  /*!
+    Updates the state of the StaticID if the StaticID is still alive.
+    It returns a value indicating if the state of StaticID changed.
+    \return Value indicating StaticID state changed.
+   */
+  bool Increment();
+  
+  //! Updates heartbeat.
+  /*!
+    Refreshes the StaticID state if the StaticID is still alive.
+   */
+  void Heartbeat();
+
+  //! Sets DynamicID.
+  /*!
+    Assigns a DynamicID to the robot.
+    \param did New DynamicID.
+   */
+  void SetDynamicID(int32_t did);
+};
---
+++ swarm_bot/src/robot.cpp
@@ -0,0 +1,56 @@
+#include "swarm_bot/robot.h"
+
+Robot::Robot() : staticID(0), dynamicID(0), heartbeat(0), missed(0), name() // zero out members
+{
+}
+Robot::Robot(std::string n, int32_t sid) : staticID(sid), dynamicID(0), heartbeat(0), missed(0), name(n)
+{
+}
+
+int32_t Robot::GetStaticID()
+{
+  return this->staticID;                          // return static id
+}
+
+int32_t Robot::GetDynamicID()
+{
+  return this->dynamicID;                         // return dynamic id
+}
+
+std::string Robot::GetName()
+{
+  return this->name;                              // return name
+}
+
+bool Robot::IsAlive()
+{
+  return this->missed <= 2;                       // return value indicating heartbeat value is acceptable
+}
+
+int32_t Robot::GetMissedHeartbeats()
+{
+  return this->missed;                            // return the amount of heartbeats missed in a row
+}
+
+bool Robot::Increment()
+{
+  this->heartbeat++;                              // increment heartbeat
+  if (this->heartbeat > 5)                        // if heartbeat exceeds interval
+    {
+      this->heartbeat = 0;                        // reset heartbeat
+      this->missed++;                             // increment missed heartbeats
+      return true;                                // return robot missed heartbeat
+    }
+  return false;                                   // return robot is still broadcasting heartbeat
+}
+
+void Robot::Heartbeat()
+{
+  this->heartbeat = 0;                            // reset heartbeat
+  this->missed = 0;                               // reset missed counter
+}
+
+void Robot::SetDynamicID(int32_t did)
+{
+  this->dynamicID = did;                          // assign new dynamic id
+}
